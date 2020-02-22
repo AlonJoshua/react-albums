7dfa8a8f4be6e6853c9ada6609d91096d6ba0109
Alon Joshua <alonjoshua@gmail.com>
2020-02-22 12:57:51 +0200

    Updates

---
+++ precache-manifest.ed134502f0f26262664f361b21d95d8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7c519b7f55e3c9d6b058ca96591de82",
+    "url": "/react-albums/index.html"
+  },
+  {
+    "revision": "034b7536c9b13cafd599",
+    "url": "/react-albums/static/css/main.19c370c3.chunk.css"
+  },
+  {
+    "revision": "75de14c8f77d9b0ac028",
+    "url": "/react-albums/static/js/2.3402d00c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-albums/static/js/2.3402d00c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "034b7536c9b13cafd599",
+    "url": "/react-albums/static/js/main.a2dd1318.chunk.js"
+  },
+  {
+    "revision": "3f067321a4e76bf2eabd",
+    "url": "/react-albums/static/js/runtime-main.5bc9520a.js"
+  }
+]);
